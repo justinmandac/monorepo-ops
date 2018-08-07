5749a5562ff8d0dd9697604cac83a899dcb14b21
justinmandac <justinmandac@gmail.com>
2018-08-07 09:17:34 +0800

    refactor(list-file-in-packages.js): Pass glob reader as an argument to listFileInPackages; exported
    Allow the glob reader to be passed as an argument for testability. Expodted globResultReducer to
    expose it to unit tests.

--- commands/list-file-in-packages/list-file-in-packages.js
+++ commands/list-file-in-packages/list-file-in-packages.js
@@ -25,16 +25,20 @@ function globResultReducer(globResults) {
   return globResults.reduce((acc, next) => acc.concat(next), []);
 }
 
+module.exports.globResultReducer = globResultReducer;
+
 /**
  * @param {string} file File to search for amongst lerna packages
  *  (ie package.json, bower.json, etc.)
  * @param {Array<string>} packagesList List of globs where lerna packages can
  *   be found.
+ * @param {function(string): Promise<Array>} reader Function that reads from
+ *   the provided glob.
  * @return {Promise<Array<string>>}
 */
-function listFileInPackages(file, packagesList) {
+function listFileInPackages(file, packagesList, reader = globPromise) {
   return Promise.all(
-    packagesList.map(package => globPromise(package))
+    packagesList.map(package => reader(package))
   )
   .then(result => result.filter(globResult => globResult.length))
   .then(globResultReducer)
